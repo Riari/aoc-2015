e7dc8fa5ba17aa07568edfbe79864afea1e4afaa
Rick Mann <3583774+Riari@users.noreply.github.com>
2023-11-17 23:06:04 +0000

    Complete day 08 part 1

---
+++ src/bin/08.rs
@@ -0,0 +1,76 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut total: u32 = 0;
+
+    for line in input.lines() {
+        let code_length: u32 = line.len() as u32;
+        let mut string_length: u32 = 0;
+
+        let chars: Vec<char> = line.chars().collect();
+        let mut skip = 0;
+        for (i, c) in chars.iter().enumerate() {
+            if i == 0 || i == line.len() - 1 {
+                continue;
+            }
+
+            if skip > 0 {
+                skip -= 1;
+                continue;
+            }
+
+            if *c != '\\' || i == line.len() - 2 {
+                string_length += 1;
+                continue;
+            }
+            
+            let next = chars.get(i + 1).unwrap();
+
+            if *next == '\\' || *next == '"' {
+                string_length += 1;
+                skip = 1;
+                continue;
+            }
+
+            if *next == 'x' && line.len() - 1 - i >= 2 {
+                if chars.get(i + 2).unwrap().is_ascii_hexdigit()
+                && chars.get(i + 3).unwrap().is_ascii_hexdigit() {
+                    string_length += 1;
+                    skip = 3;
+                    continue;
+                }
+            }
+
+            string_length += 1;
+        }
+
+        total += code_length - string_length;
+    }
+
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 8);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 8);
+        assert_eq!(part_one(&input), Some(12));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 8);
+        assert_eq!(part_two(&input), None);
+    }
+}
